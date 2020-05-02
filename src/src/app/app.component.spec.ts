94c996405b315b5f41805f512facf8f912b9a889
emidiomorgia <emidio@morgia.info>
2020-05-02 19:55:43 +0200

    Travis ci

--- src/src/app/app.component.spec.ts
+++ src/src/app/app.component.spec.ts
@@ -20,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'electron-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('electron-test');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('electron-test app is running!');
-  });
 });
